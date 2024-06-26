8190d85a845241921e63a9ce0ff90987d2505755
badjngler <lukaskiriakakis@gmail.com>
2024-06-26 16:09:31 +0200

    scedule Zeit angepasst und neue activity für die tabelle

---
+++ app/src/main/java/de/hka/zaelfahrtendisposition/scedule_table.java
@@ -0,0 +1,14 @@
+package de.hka.zaelfahrtendisposition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class scedule_table extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scedule_table);
+    }
+}

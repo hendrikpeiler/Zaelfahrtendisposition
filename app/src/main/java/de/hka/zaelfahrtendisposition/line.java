0f60078f2244c23b4cdf5ae50fb128ec3d258c6e
badjngler <lukaskiriakakis@gmail.com>
2024-06-26 02:19:59 +0200

    activities hinzugefügt und gird Layout hinzugefügt

---
+++ app/src/main/java/de/hka/zaelfahrtendisposition/line.java
@@ -0,0 +1,14 @@
+package de.hka.zaelfahrtendisposition;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class line extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_line);
+    }
+}
